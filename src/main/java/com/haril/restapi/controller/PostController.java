74551237cf1ebe8c311e51fdc39d83fed55237f6
Haril Song <songkg7@gmail.com>
2021-05-18 22:44:14 +0900

    POST 요청을 보내는 다양한 방법 3 - @JsonProPerty

--- src/main/java/com/haril/restapi/controller/PostController.java
+++ src/main/java/com/haril/restapi/controller/PostController.java
@@ -10,12 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
 
 /**
-* @package com.haril.restapi.controller
-* @name PostController.java
-* @since 2021/05/18 10:29 오후
-* @author songkg7
-* @version 1.0.0
-*/
+ * @author songkg7
+ * @version 1.0.0
+ * @package com.haril.restapi.controller
+ * @name PostController.java
+ * @since 2021/05/18 10:29 오후
+ */
 
 @Slf4j
 @RestController
@@ -25,8 +25,8 @@ public class PostController {
     /**
      * POST 요청을 보내는 다양한 방법 1
      */
-    @PostMapping("/post")
-    public void post(@RequestBody Map<String, Object> requestData) {
+    @PostMapping("/post01")
+    public void post01(@RequestBody Map<String, Object> requestData) {
 
         requestData.forEach((key, value) -> {
             System.out.println("entry.getKey() = " + key);
@@ -35,9 +35,12 @@ public class PostController {
     }
 
     // POST 요청을 보내는 다양한 방법 2 - DTO 활용
-    @PostMapping("/post")
-    public void post(@RequestBody PostRequest requestData) {
+    // POST 요청을 보내는 다양한 방법 3 - 주고받는 JSON data 가 스네이크케이스와 카멜케이스로 서로 달라서 매칭이 안되는 경우 해결 방법
+    // @JsonProperty 활용
+    @PostMapping("/post02")
+    public void post02(@RequestBody PostRequest requestData) {
         System.out.println(requestData);
     }
+
 
 }
--- src/main/java/com/haril/restapi/dto/PostRequest.java
+++ src/main/java/com/haril/restapi/dto/PostRequest.java
@@ -1,5 +1,6 @@
 package com.haril.restapi.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 /**
@@ -13,4 +14,10 @@ public class PostRequest {
     private String password;
     private String email;
     private String address;
+
+    @JsonProperty("phone_number")
+    private String phoneNumber; // phone_number
+
+    @JsonProperty("OTP")
+    private String OTP; // camel 도 아니고 snakeCase 도 아닌 경우
 }
